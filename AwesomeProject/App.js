04363a042d80f983bd8f3a96ec6dede54700b3a6
Pio Molina <vpio@comcast.net>
2018-12-08 22:22:53 -0500

    initial commit

---
+++ AwesomeProject/App.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import { StyleSheet, Text, View, PanResponder, Animated, AsyncStorage } from 'react-native';
+import { Divider, Header, Button } from 'react-native-elements';
+import axios from 'axios';
+
+export default class App extends React.Component {
+  state = {
+    toggle: false,
+    pan: new Animated.ValueXY(),
+    hex: '',
+    colorArr: []
+  }
+
+  componentWillMount(){
+    axios.get('http://localhost:3001/api/v1/colors.json')
+          .then(response => {
+              console.log("hello it worked *******", response)
+              // this.setState({
+              //     lists: response.data
+              // })
+          })
+          .catch(error => console.log("hello please work **********", error))
+
+    this._val = { x:0, y:0 }
+    this.state.pan.addListener((value) => this._val = value);
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gesture) => true,
+      onPanResponderMove: Animated.event([
+        null, { dx: this.state.pan.x, dy: this.state.pan.y }
+      ]),
+      onPanResponderGrant: (e, gestureState) => {
+        // Set the initial value to the current state
+        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+        this.state.pan.setValue({x: 0, y: 0});
+      },
+      onPanResponderRelease: (e, gesture) => {
+         // this.state.pan.flattenOffset();
+       Animated.spring(this.state.pan, {
+         toValue: { x: 0, y: 0 },
+         friction: 5
+       }).start();
+     }
+    });
+    this.state.pan.setValue({ x:0, y:0})
+  }
+
+  getRandomColor = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    this.setState({hex: color})
+    console.log("here is the hex: ", this.state.hex)
+    return color;
+  }
+
+  saveColor = () => {
+    let {hex} = this.state
+    let {colorArr} = this.state
+    let addedColor = this.state.colorArr.concat(hex)
+    this.setState({colorArr: addedColor})
+    AsyncStorage.setItem("myKey", JSON.stringify(colorArr))
+        // this.setState({"myKey": hex});
+    // console.log("hi")
+    // axios.post('http://localhost:3001/api/v1/colors.json', {color: {hex_code: hex}})
+  }
+
+  showColor = () => {
+    AsyncStorage.getItem("myKey").then((value) =>
+           JSON.parse(value))
+           .then(json => {
+             this.setState({colorArr: json})
+           })
+           .catch(error => console.log('error!'))
+       .done();
+  }
+
+  render() {
+    let {hex} = this.state
+    const panStyle = {
+      transform: this.state.pan.getTranslateTransform()
+    }
+    return (
+      <React.Fragment>
+      <View>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: `Pio's Dope app`, style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+          />
+      </View>
+      <View style={[styles.container, this.state.toggle && {'backgroundColor': hex}]} >
+        {/*<Animated.View
+            {...this.panResponder.panHandlers}
+            style={[panStyle, styles.circle]}
+          />*/}
+        <Button onPress={()=> {
+            this.setState({toggle: true})
+            this.getRandomColor()
+        }}
+        title='Change!'/>
+        <Button onPress={()=> this.saveColor()} title='Save This Color'/>
+        <Button onPress={()=> this.showColor()} title='Show Last Saved'/>
+        <Divider style={{ backgroundColor: 'blue' }} />
+        <Text>
+          {this.state.colorArr}
+        </Text>
+      </View>
+    </React.Fragment>
+    );
+  }
+}
+
+let CIRCLE_RADIUS = 30;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  circle: {
+    backgroundColor: "skyblue",
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    borderRadius: CIRCLE_RADIUS
+  }
+  // altColor: {
+  //   backgroundColor: getRandomColor()
+  // },
+});
