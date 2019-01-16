888094f8b8bcb6f58feef1ac3399cc6669820739
Pio Molina <vpio@comcast.net>
2019-01-16 02:51:50 -0500

    reformatted menu so state is in app and login is the first thing you see

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_193323) do
+ActiveRecord::Schema.define(version: 2019_01_16_074316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_01_15_193323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", null: false
+    t.datetime "last_login"
+    t.string "role", default: "user", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
